b062d0b1ebe371dbbfeb07006406d8e7b65f62d6
BjarkeBP <Bjarkebp@live.dk>
2024-11-11 12:47:15 +0100

    update the project service

--- services/models/Project.ts
+++ services/models/Project.ts
@@ -1,22 +1,27 @@
 ﻿/**
  * A class representing a project
  */
-export class Project
-{
-    public id: string;
-    public title: string;
-    public status: string;
-    public priority: string;
-    public timeRange: Date[];
-    public containedIn: string;
+export class Project {
+  public id: string;
+  public title: string;
+  public containedIn: string;
+  public status: string;
+  public priority: string;
+  public timeRange: Date[];
 
-    constructor(id: string, title: string, status: string, priority: string, timeRange: Date[], containedIn: string)
-    {
-        this.id = id;
-        this.title = title;
-        this.status = status;
-        this.priority = priority;
-        this.timeRange = timeRange;
-        this.containedIn = containedIn;
-    }
+  constructor(
+    id: string,
+    title: string,
+    containedIn: string,
+    status: string,
+    priority: string,
+    timeRange: Date[]
+  ) {
+    this.id = id;
+    this.title = title;
+    this.containedIn = containedIn;
+    this.status = status;
+    this.priority = priority;
+    this.timeRange = timeRange;
+  }
 }
